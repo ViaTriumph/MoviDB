3aa7bad6e2f2440aec9699e8910a587a39bbb886
DP <avidtriumph@gmail.com>
2022-02-20 15:22:48 +0530

    Initial commit

---
+++ app/src/main/java/com/practice/movidb/di/AppComponent.kt
@@ -0,0 +1,8 @@
+package com.practice.movidb.di
+
+import dagger.Component
+
+@Component(modules = [NetworkMo])
+interface AppComponent {
+
+}
---
+++ network/src/main/java/com/practice/network/di/NetworkModule.kt
@@ -0,0 +1,7 @@
+package com.practice.network.di
+
+import dagger.Module
+
+@Module
+interface NetworkModule {
+}
